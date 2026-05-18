733aaf6db2482a289b3a45c79638daaa82de5fc2
lisa maiorano <lisamaiorano07@gmail.com>
2026-05-18 07:36:26 +0200

    version 1.1

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "INSERISCI_API_KEY",
+  authDomain: "INSERISCI_PROJECT_ID.firebaseapp.com",
+  projectId: "INSERISCI_PROJECT_ID",
+  storageBucket: "INSERISCI_PROJECT_ID.appspot.com",
+  messagingSenderId: "INSERISCI_MESSAGING_SENDER_ID",
+  appId: "INSERISCI_APP_ID"
+};
+
+export const syncDocumentPath = "studyPlans/main";
